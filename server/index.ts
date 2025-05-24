6639bfd30b21fd58f99bb1f4797207ee74315b05
OkechukwuMiracle <damianmiracle18@gmail.com>
2025-05-24 14:13:11 +0200

    updated

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
+import cors from 'cors';
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: 'https://bolreckitt.vercel.app',
+  credentials: true // if you are using cookies
+}));
 
 app.use((req, res, next) => {
   const start = Date.now();
